4aa111483908bc62611674b41bcbc2199963bad1
jofercho <ssoj.josss@gmail.com>
2025-09-02 19:13:54 -0500

    separating from backend

--- server/routes/paintingRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');;
-
-const router = express.Router();
-const paintingController = require('../controllers/paintingController');
-
-router.get('/paintings', paintingController.getPaintings);
-
-module.exports = router;
